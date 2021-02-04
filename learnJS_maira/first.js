49dbfca85a1b589ecae78b7f69db02af4467f09b
mairako <mkotsovoulou@acg.edu>
2021-02-04 15:25:09 +0000

    exerciseJs2

--- learnJS_maira/first.js
+++ learnJS_maira/first.js
@@ -1,4 +1,17 @@
 console.log("Javascript ruinning");
+var text = "start counting!!!";
+var i=0;
+while (i < 10) {
+   text +="<br>count to: " + i;
+   i++
+}
+document.getElementById('numbers').innerHTML = text;
+
+text="";
+for (i=0; i<10; i++) {
+   text +="<br>count to: " + i;
+}
+
 function hide() {
   console.log("hide function called");
   document.getElementById('description').style.visibility='hidden';
