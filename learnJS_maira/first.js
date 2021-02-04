dc3c78c86a23169344117d691df4e3cdd778d821
mairako <mkotsovoulou@acg.edu>
2021-02-04 14:17:32 +0000

    Javascript 3

--- learnJS_maira/first.js
+++ learnJS_maira/first.js
@@ -1,4 +1,19 @@
-
+console.log("Javascript ruinning");
 function hide() {
+  console.log("hide function called");
   document.getElementById('description').style.visibility='hidden';
+}
+
+function change_image(){
+  console.log("The current image is: " + document.getElementById('logo').src);
+  var imageSrc = document.getElementById('logo').src;
+  imageSrc = imageSrc.replace('https://bwpjlp09.apps.lair.io/learnJS_maira/','');
+  console.log("The current image stripped is: " + imageSrc);
+  
+  if (imageSrc =="ecma.png") {
+      document.getElementById('logo').src="jay.png";
+  } else
+    {
+      document.getElementById('logo').src="ecma.png";
+    }
 }
